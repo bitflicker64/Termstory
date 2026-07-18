64980d6c04285544c2a02729b112598fd22fcf9f
Himanshu Verma <himnshuverma10152006@gmail.com>
2026-07-18 20:36:35 +0530

    test(miku): smoke C multi-file unused import + div

---
+++ termstory/_miku_sc_c1.py
@@ -0,0 +1,5 @@
+"""Smoke C file1. Delete after test."""
+import sys  # unused
+
+def twice(n: int) -> int:
+    return n * 2
---
+++ termstory/_miku_sc_c2.py
@@ -0,0 +1,4 @@
+"""Smoke C file2. Delete after test."""
+
+def div_safe(a: int, b: int) -> float:
+    return a / b  # missing zero check mechanical-ish
